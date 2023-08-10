b0f482fe061f4fbe9ba14db9ec167f7c1d7cbeb6
Ktos17 <132001347+Ktos17@users.noreply.github.com>
2023-08-10 12:38:19 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('message', (message) => {
+    io.emit('message', message); // Wysyła wiadomość do wszystkich klientów
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
